1c6095b5f90e0e38577f0559da6e3263a94a0c34
onlyiasolutions <onlyiasolutions@gmail.com>
2026-03-30 04:23:32 +0200

    Initial Commit

---
+++ noteOS/noteOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteOSApp.swift
+//  noteOS
+//
+//  Created by Gorka on 30/3/26.
+//
+
+import SwiftUI
+
+@main
+struct noteOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
